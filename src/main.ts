6e96c09bb6be177afc7fc33c18a566b3b6fe6b16
张春鸣 <12629241+zhang-chunmingx5@user.noreply.gitee.com>
2025-09-12 17:15:09 +0800

    删除src内容

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import './assets/main.css'
+// import './assets/main.css'
+import { createApp } from 'vue'// 引入createApp用于创建应用
+import App from './App.vue'// 引入根组件App.vue
+// import { createPinia } from 'pinia'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+// import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+// app.use(createPinia())
+// app.use(router)
 
 app.mount('#app')
