9d4e1f66e96669b7828b48ae9296c1f626f2658d
张春鸣 <12629241+zhang-chunmingx5@user.noreply.gitee.com>
2025-09-12 17:39:43 +0800

    person.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-<div class="app">1</div>
+<div class="app">
+  <Person/>
+</div>
 </template>
+
 <script lang="ts">
+// import Person from './components/Person.vue'; // 明确写扩展名
+import Person from './components/Person.vue'
 export default{
-  name:'App'
+  name:'App',
+  components:{Person}
 }
 </script>
 <style scoped>
---
+++ src/components/Person.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Person',
+  components: {},
+  props: {},
+  data() {
+    return {
+      message: 'Hello Vue 2!',
+    }
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+}
+</script>
+
+<style scoped></style>
